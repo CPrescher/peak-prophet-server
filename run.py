8471d162231b6b8fe48555a1709b600404b289b2
Clemens Prescher <clemens.prescher@gmail.com>
2023-03-10 15:16:01 +0100

    change to uvicorn asgi server, due to sanic causing errors upon closing the connection

--- run.py
+++ run.py
@@ -1,15 +1,31 @@
+
+import uvicorn
 import socketio
-from sanic import Sanic
 from peak_prophet_server.sessions import session_manager as sm
 from peak_prophet_server.sio_events import connect_events
 
-sio = socketio.AsyncServer(async_mode='sanic', cors_allowed_origins="*")
+
+############################################
+# OLD WAY to start server:
+# sio = socketio.AsyncServer(async_mode='sanic', cors_allowed_origins="*")
+# connect_events(sio, sm)
+#
+#
+# app = Sanic("Peak-Prophet-Server")
+# sio.attach(app)
+#
+# if __name__ == '__main__':
+#     app.run()
+
+############################################
+
+
+sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins="*")
 connect_events(sio, sm)
 
-
-app = Sanic("Peak-Prophet-Server")
-sio.attach(app)
+app = socketio.ASGIApp(sio)
 
 if __name__ == '__main__':
-    app.run()
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
 
